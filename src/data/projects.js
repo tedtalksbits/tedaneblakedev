69d8b4c4ebf18cf51ac4f7749c57f1e3109d952d
tedtalksbits <tedaneblake@gmail.com>
2022-03-03 14:39:37 -0500

    :star: new projects added

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,66 +1,97 @@
-import projectTwoImg from '../images/project2.jpg'
-import projectThreeImg from '../images/project3.jpg'
-import projectFourImg from '../images/project4.jpg'
+import projectTwoImg from "../images/project2.jpg";
+import projectThreeImg from "../images/project3.jpg";
+import projectFourImg from "../images/project4.jpg";
+import project5Img from "../images/project5.gif";
+import project6Img from "../images/project6.png";
 
 export const projectsData = [
    {
-      preview: 'https://github.com/tedtalksbits/us-citizenship-quiz/raw/main/src/static/quiz-gif.gif',
-      title: 'US Citizen Quiz',
+      preview: project6Img,
+      title: "blkidscode.co",
+      techs: ["Javascript", "css3", "HTML5"],
+      link: "https://github.com/tedtalksbits/blkidcode",
+      id: "left_card",
+      desc: "The starter layout of a collaboration project",
+      type: "Web App",
+      demo: "https://blkidscode.co",
+   },
+   {
+      preview: project5Img,
+      title: "MacOS Monterey In Javascript",
+      techs: ["Javascript", "css3", "HTML5"],
+      link: "https://github.com/tedtalksbits/macOS",
+      id: "left_card",
+      desc: "Building the latest version of MacOS using only vanilla Javascript and css",
+      type: "Web App",
+      demo: "",
+   },
+   {
+      preview:
+         "https://github.com/tedtalksbits/us-citizenship-quiz/raw/main/src/static/quiz-gif.gif",
+      title: "US Citizen Quiz",
       techs: ["react", "styled-components"],
-      link: 'https://github.com/tedtalksbits/us-citizenship-quiz',
+      link: "https://github.com/tedtalksbits/us-citizenship-quiz",
       id: "left_card",
       desc: "US Citizenship Quiz: 28 Common Question directly from USCIS",
       type: "Web App",
-      demo: "https://tedtalksbits.github.io/us-citizenship-quiz/"
+      demo: "https://tedtalksbits.github.io/us-citizenship-quiz/",
    },
    {
-      preview: 'https://github.com/tedtalksbits/cleartasks/raw/main/src/screenshots/ct-gif-custom.gif',
-      title: 'ClearTasks - Notion Inspired',
-      techs: ["react", "styled-components", "Node JS", "REST API", "MongoDB", "Firebase"],
-      link: 'https://github.com/tedtalksbits/cleartasks',
+      preview:
+         "https://github.com/tedtalksbits/cleartasks/raw/main/src/screenshots/ct-gif-custom.gif",
+      title: "ClearTasks - Notion Inspired",
+      techs: [
+         "react",
+         "styled-components",
+         "Node JS",
+         "REST API",
+         "MongoDB",
+         "Firebase",
+      ],
+      link: "https://github.com/tedtalksbits/cleartasks",
       id: "left_card",
       desc: " ClearTasks © is a task manager that helps you stay productive and organized by separating your tasks.",
       type: "Web App",
-      demo: "https://tedtalksbits.github.io/cleartasks/#/cleartasks/"
+      demo: "https://tedtalksbits.github.io/cleartasks/#/cleartasks/",
    },
    {
-      preview: 'https://i.postimg.cc/CMtQD6TF/bloggio.gif',
-      title: 'React Blog',
+      preview: "https://i.postimg.cc/CMtQD6TF/bloggio.gif",
+      title: "React Blog",
       techs: ["react", "styled-components", "Node JS", "REST API", "MongoDB"],
-      link: 'https://github.com/tedtalksbits/react-blog',
+      link: "https://github.com/tedtalksbits/react-blog",
       id: "left_card",
       desc: "A blog built with react that showcases many of the basic principles of a modern web app.",
       type: "Web App",
-      demo: ""
+      demo: "",
    },
    {
       preview: projectTwoImg,
-      title: 'Custom UI Elements',
+      title: "Custom UI Elements",
       techs: ["react", "styled-components", "react hooks", "Custom Theming"],
-      link: 'https://github.com/tedtalksbits/react-dashboard-refactor',
-      id: 'center_card',
+      link: "https://github.com/tedtalksbits/react-dashboard-refactor",
+      id: "center_card",
       desc: "Using custom themes, this basic app was done to show case color theory as well as theme switching.",
       type: "Web App",
-      demo: "http://tedtalksbits.github.io/react-dashboard-refactor"
+      demo: "http://tedtalksbits.github.io/react-dashboard-refactor",
    },
    {
       preview: projectThreeImg,
-      title: 'Modern Blackboard',
+      title: "Modern Blackboard",
       techs: ["react", "styled-components", "Rest API", "JSON Server"],
-      link: 'https://github.com/tedtalksbits/modern-blackboard',
-      id: 'right_card',
+      link: "https://github.com/tedtalksbits/modern-blackboard",
+      id: "right_card",
       desc: "This is a personal project that aims to eleviate the eye strain on my college's official blackboard.",
       type: "Web App",
-      demo: "https://tedtalksbits.github.io/modern-blackboard"
+      demo: "https://tedtalksbits.github.io/modern-blackboard",
    },
    {
       preview: projectFourImg,
-      title: 'Social Media',
+      title: "Social Media",
       techs: ["react", "Rest API", "MongoDB", "redux", "material-ui"],
-      link: 'https://github.com/tedtalksbits/memories_fullstack',
-      id: 'right_card',
+      link: "https://github.com/tedtalksbits/memories_fullstack",
+      id: "right_card",
       desc: "Building this app helped me explore CRUD more in detail and also learn about redux and material ui. ",
       type: "Web App",
-      demo: "https://emandtedblake.netlify.app"
+      demo: "https://emandtedblake.netlify.app",
    },
-]
+];
--- src/pages/Testimonials.js
+++ src/pages/Testimonials.js
@@ -1,24 +1,30 @@
-import React from 'react'
-import Testimonial from '../components/TestimonialCard/Testimonial'
-import { MainContentWrapper, ResponsiveGrid } from '../components/utils/mainComponents'
-import { themeColors } from '../data/appColors'
-import { Heading } from './InfoSection'
-import Fade from 'react-reveal/Fade';
+import React from "react";
+import Testimonial from "../components/TestimonialCard/Testimonial";
+import {
+   MainContentWrapper,
+   ResponsiveGrid,
+} from "../components/utils/mainComponents";
+import { themeColors } from "../data/appColors";
+import { Heading } from "./InfoSection";
+import Fade from "react-reveal/Fade";
 const Testimonials = () => {
    return (
-      <MainContentWrapper background={themeColors.page} id="testimonial" className="section testimonials">
-         <Heading>Testimonials</Heading><small>in progress...</small>
+      <MainContentWrapper
+         background={themeColors.page}
+         id="testimonial"
+         className="section testimonials"
+      >
+         <Heading>Testimonials</Heading>
+         <small>in progress...</small>
          <ResponsiveGrid>
             <Fade top>
-
                <Testimonial />
             </Fade>
             <Testimonial />
             <Testimonial />
          </ResponsiveGrid>
-
       </MainContentWrapper>
-   )
-}
+   );
+};
 
-export default Testimonials
+export default Testimonials;
